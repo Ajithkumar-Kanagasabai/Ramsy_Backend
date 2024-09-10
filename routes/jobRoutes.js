9580f449a26514b316e314a2a7d028304eddf3ad
Ajith-Kanagasabai <ajithkanagasabai17@gmail.com>
2024-09-10 19:50:37 +0530

    Job

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -8,4 +8,25 @@ const jobController = require('../controllers/jobController');
 router.get('/', jobController.getAllJobs);
 router.get('/:id', jobController.getAJob);
 
+router.post('/exchange-code', async (req, res) => {
+    const { code } = req.body;
+    const clientId = '18645e40-e9b9-4e41-ae94-1a7f68772a73';// Replace with your actual client secret
+    const redirectUri = 'https://ramsy-e3eaa.web.app/JobsPage';
+    const tokenUrl = 'https://id.vincere.io/oauth2/token';
+  
+    try {
+      const response = await axios.post(tokenUrl, new URLSearchParams({
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+      }));
+      
+      res.json(response.data); // Contains access token and other information
+    } catch (error) {
+      console.error('Error exchanging code for token:', error);
+      res.status(500).json({ error: 'Failed to exchange code for token' });
+    }
+  });
+
 module.exports = router;
